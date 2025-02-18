f65d68d4eb871e845eaf3a70fcfdd7eea29d2f9e
Kay Hsu <kayhsu@Kays-MacBook-Pro.local>
2025-02-18 15:04:33 -0800

    update2

--- employee-test-v2/server.cjs
+++ employee-test-v2/server.cjs
@@ -64,14 +64,14 @@ app.post(`/employees/post`, (req, res, next) => {
 });
 
 // Error Message
-app.use((error, req, res, next) => {
+app.use((req, res, next) => {
   console.log(`ERROR MESSAGE`, err.message);
   res.status(400).send(err.message);
 });
 
 
 // 404 page not found
-app.use((res, req) => {
+app.use((req, res, next) => {
   res.status(404).send(`Page not found`)
 });
 
